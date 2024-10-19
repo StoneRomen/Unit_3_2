ba2b04314899b30da30fe8487d7166f198e7cfd0
StoneRomen <184683610+StoneRomen@users.noreply.github.com>
2024-10-19 15:02:28 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+// Отримати від користувача 3 рядки та вивести їх у довільному порядку однією командою (шаблонні рядки);
+
+
+const str1 = 'JavaScript';
+const str2 = 'know';
+const str3 = 'I dont';
+
+const strings = [str1, str2, str3];
+
+console.log (strings[2],strings[1], strings[0],)
